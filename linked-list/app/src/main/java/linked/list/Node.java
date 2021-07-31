bb6ea839d1a307b5981190b26fddba333ca021b6
baraaAlosaily <seelbaraa@gmail.com>
2021-07-31 04:28:01 -0700

    done

---
+++ linked-list/app/src/main/java/linked/list/Node.java
@@ -0,0 +1,11 @@
+package linked.list;
+//Create node class
+public class Node {
+    //Node structure
+    int data;
+    Node next;
+//Node constructor
+    public Node(int data) {
+        this.data=data;
+    }
+}
