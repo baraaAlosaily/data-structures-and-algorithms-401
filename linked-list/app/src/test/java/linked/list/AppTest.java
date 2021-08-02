c7f7c6aabf3c4a9f07b26f45870bce84de8ad4d1
baraaAlosaily <seelbaraa@gmail.com>
2021-08-02 03:44:04 +0300

    done

--- linked-list/app/src/test/java/linked/list/AppTest.java
+++ linked-list/app/src/test/java/linked/list/AppTest.java
@@ -26,8 +26,8 @@ class AppTest {
         list.insert(15);
         list.insert(20);
         list.insert(25);
-        assertEquals(25,list.head.data);
-        assertEquals("{25} -> {20} -> {15} -> {10} -> {5} -> Null",list.show());
+        assertEquals(5,list.head.data);
+        assertEquals("{5} -> {10} -> {15} -> {20} -> {25} -> Null",list.show());
     }
     //(test5)return true when finding a value within the linked list that exists
     //(test6)return false when searching for a value in the linked list that does not exist
@@ -60,6 +60,67 @@ class AppTest {
         assertEquals(5,list.head.data);
         assertEquals(3,list2.head.data);
         assertEquals(3,list3.head.data);
+    }
+    //Code Challenge (6)
+    //test 1:add a node to the end of the linked list
+    @Test void addNode(){
+        LinkedList list=new LinkedList();
+        list.insert(5);
+        list.insert(10);
+        list.insert(15);
+        list.append(30);
+        assertEquals("{5} -> {10} -> {15} -> {30} -> Null",list.show());
+    }
+    //test 2:add multiple nodes to the end of a linked list
+    @Test void addMutipleNote(){
+        LinkedList list=new LinkedList();
+        list.insert(5);
+        list.insert(10);
+        list.insert(15);
+        list.append(28);
+        list.append(33);
+        list.append(36);
+        assertEquals("{5} -> {10} -> {15} -> {28} -> {33} -> {36} -> Null",list.show());
+    }
+    //test 3: insert a node before a node located i the middle of a linked list
+    @Test void insertBesfore(){
+        LinkedList list=new LinkedList();
+        list.insert(5);
+        list.insert(10);
+        list.insert(15);
+        list.insertbefore(15,45);
+        assertEquals("{5} -> {10} -> {45} -> {15} -> Null",list.show());
+
+    }
+    //test4 insert a node before the first node of a linked list
+    @Test void insertBesforeFirstNode(){
+        LinkedList list=new LinkedList();
+        list.insert(5);
+        list.insert(10);
+        list.insert(15);
+        list.insertbefore(5,45);
+        assertEquals("{45} -> {5} -> {10} -> {15} -> Null",list.show());
+
+    }
+    //test 5 insert after a node in the middle of the linked list
+    @Test void insertAfterNode(){
+        LinkedList list=new LinkedList();
+        list.insert(5);
+        list.insert(10);
+        list.insert(15);
+        list.insert(45);
+        list.insertAfter(10,35);
+        assertEquals("{5} -> {10} -> {35} -> {15} -> {45} -> Null",list.show());
+    }
+    //test 6 insert after a last node in the middle of the linked list
+    @Test void insertAfterLastNode(){
+        LinkedList list=new LinkedList();
+        list.insert(5);
+        list.insert(10);
+        list.insert(15);
+        list.insert(45);
+        list.insertAfter(45,35);
+        assertEquals("{5} -> {10} -> {15} -> {45} -> {35} -> Null",list.show());
     }
 
 }
