5db1ee831582956747924c04eb1d16d06ac55dba
baraaAlosaily <seelbaraa@gmail.com>
2021-08-13 01:53:29 +0300

    done

---
+++ trees/app/src/main/java/trees/Node.java
@@ -0,0 +1,12 @@
+package trees;
+public class Node {
+    int data;
+    Node leftChild=null;
+    Node rightChild=null;
+
+    public Node(int data) {
+        this.data = data;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+}
